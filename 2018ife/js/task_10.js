e527d99eefe8ff90ac1d30bfadb38630a12627ad
Chengliang Cai <dev.chua@gmail.com>
2018-06-21 22:55:53 +0800

    task10

--- 2018ife/js/task_10.js
+++ 2018ife/js/task_10.js
@@ -334,3 +334,137 @@ console.log(removeRepetition("abbba")); // ->aba
 console.log(removeRepetition("aabbaabb")); // ->abab
 console.log(removeRepetition("")); // ->
 console.log(removeRepetition("abc")); // ->abc
+
+//任务四
+var tree = {
+    "id": 0,
+    "name": "root",
+    "left": {
+        "id": 1,
+        "name": "Simon",
+        "left": {
+            "id": 3,
+            "name": "Carl",
+            "left": {
+                "id": 7,
+                "name": "Lee",
+                "left": {
+                    "id": 11,
+                    "name": "Fate"
+                }
+            },
+            "right": {
+                "id": 8,
+                "name": "Annie",
+                "left": {
+                    "id": 12,
+                    "name": "Saber"
+                }
+            }
+        },
+        "right": {
+            "id": 4,
+            "name": "Tony",
+            "left": {
+                "id": 9,
+                "name": "Candy"
+            }
+        }
+    },
+    "right": {
+        "id": 2,
+        "name": "right",
+        "left": {
+            "id": 5,
+            "name": "Carl",
+        },
+        "right": {
+            "id": 6,
+            "name": "Carl",
+            "right": {
+                "id": 10,
+                "name": "Kai"
+            }        
+        }
+    }
+}
+
+// 假设id和name均不会重复，根据输入name找到对应的id
+function findIdByName(obj, name) { 
+    function findName(obj) {
+        for (i in obj) {
+            if (typeof obj[i] == "object") {
+                findName(obj[i]);
+            } else if (obj[i] == name) {
+                console.log(obj.id);
+            }
+        }
+    }
+    findName(obj);
+}
+findIdByName(tree, "Simon");
+
+// 假设id和name均不会重复，根据输入id找到对应的name
+function findNameById(obj, id) {
+    function findId(obj) {
+        for (i in obj) {
+            if (typeof obj[i] == "object") {
+                findId(obj[i]);
+            } else if (obj[i] == id) {
+                console.log(obj.name);
+            }
+        }
+    }
+    findId(obj);
+}
+findNameById(tree, 4);
+
+// 把这个对象中所有的名字以“前序遍历”的方式全部输出到console中，根左右
+function getListWithDLR(obj) {
+    var nameList = "";
+    function findName(obj) {
+        for (i in obj) {
+            //console.log(i);
+            if (obj[i] == obj.name) {
+                nameList += obj[i] + " ";
+            } else if (obj[i] == obj.left) {
+                findName(obj[i]);
+            } else if (obj[i] == obj.right) {
+                findName(obj[i]);
+            }
+        }
+        
+    }
+    findName(obj);
+    console.log(nameList);
+}
+getListWithDLR(tree);
+
+// 把这个对象中所有的名字以“中序遍历”的方式全部输出到console中，左根右
+function getListWithLDR(obj) {
+    var nameList = "";
+    function findName(obj) {
+        for (i in obj) {
+            if (obj[i] == obj.left) {
+                findName(obj[i]);
+                if (obj[i] == obj.name) {
+                    nameList += obj[i].name + " ";
+                }
+                
+            } else if (obj[i] == obj.right) {
+                findName(obj[i]);
+                if (obj[i] == obj.name) {
+                    nameList += obj[i].name + " ";
+                }
+            }
+        }
+    }
+    findName(obj);
+    console.log(nameList);
+}
+getListWithLDR(tree);
+
+// 把这个对象中所有的名字以“后序遍历”的方式全部输出到console中，左右根
+function getListWithLRD() {
+
+}
