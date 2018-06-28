b3eb252b401cb44c832105ad6775b9a32f3ad484
Chengliang Cai <dev.chua@gmail.com>
2018-06-28 23:26:36 +0800

    task11

---
+++ 2018ife/js/task11.js
@@ -0,0 +1,60 @@
+
+function showTime() {
+    var nowDate = new Date();
+    document.getElementById("result").innerHTML = Date();
+}
+setInterval("showTime()", 1000);
+
+// var nowYear = nowDate.getFullYear();
+// var nowMonth = nowDate.getMonth();
+// var nowDay = nowDate.getDate();
+// var nowWeekDay = "";
+
+function showTime2() {
+    var nowDate = new Date(); 
+    var nowYear = nowDate.getFullYear();
+    var nowMonth = nowDate.getMonth();
+    var nowDay = nowDate.getDate();
+    var nowWeekDay = "";
+    var nowHour = nowDate.getHours();
+    var nowMinute = nowDate.getMinutes();
+    var nowSecond = nowDate.getSeconds();
+    switch(nowDate.getDay()) {
+        case 0:
+            nowWeekDay = "星期天";
+            break;
+        case 1:
+            nowWeekDay = "星期一";
+            break;
+        case 2:
+            nowWeekDay = "星期二";
+            break;
+        case 3:
+            nowWeekDay = "星期三";
+            break;
+        case 4:
+            nowWeekDay = "星期四";
+            break;
+        case 5:
+            nowWeekDay = "星期五";
+            break;
+        case 6:
+            nowWeekDay = "星期六";
+            break;
+    }
+    nowMonth = checkTime(nowMonth);
+    nowDay = checkTime(nowDay);
+    nowMinute = checkTime(nowMinute);
+    nowHour = checkTime(nowHour);
+    nowSecond = checkTime(nowSecond);
+    document.getElementById("result2").innerHTML = nowYear + "年" + nowMonth + "月" + nowDay + "日" + " " + nowWeekDay + " " + nowHour + ":" + nowMinute + ":" + nowSecond;
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;      
+    }  
+    return i;
+}
+
+setInterval("showTime2()", 1000);
